2496090d17b362a0e78b188510f0c261139e5398
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-09-02 16:20:21 -0400

    add: users endpoint test

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
+import { verifyToken } from "../middlewares/accessVerification";
+import { signup, getUser } from "../controllers/connection";
 import { Router } from "express";
-import { signup } from "../controllers/connection";
 
 const route = Router();
 
 // * register into the service
 route.post("/connect", signup);
 
-
+route.get("/user", verifyToken, getUser);
 export default route;
