b14f475421f73baf7a48f1df1da2dff3a7f94984
Gil Rudolf Härdi <ghaerdi@ipisdosaj.edu.do>
2020-09-02 01:21:32 -0400

    add: JWT Passport

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ if (!process.env.PORT) {
 import server from "./app";
 import "./clear";
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT;
 
 server.listen(port);
 console.log(`server on port: ${port}`);
